4f6e1bc4a1daa82a2f6c1b89a9a64bcaf427e97c
alihitawala <Kingnadal1>
2016-12-10 17:07:17 -0600

    Added Big forward and even test cases

--- src/main.cpp
+++ src/main.cpp
@@ -66,6 +66,8 @@ BufMgr *bufMgr = new BufMgr(100);
 // -----------------------------------------------------------------------------
 
 void createRelationForward();
+void createRelationForwardEven();
+void createRelationForwardBig();
 
 void createRelationBackward();
 
@@ -74,6 +76,7 @@ void createRelationRandomBig();
 void createRelationNegative();
 
 void intTests();
+void intTestsEven();
 void intTestsBig();
 void intTestsNegative();
 void intTestsComplex();
@@ -81,6 +84,7 @@ void intTestsComplex();
 int intScan(BTreeIndex *index, int lowVal, Operator lowOp, int highVal, Operator highOp);
 
 void indexTests();
+void indexTestsEven();
 void indexTestsBig();
 void indexTestsNegative();
 void indexTestsComplex();
@@ -93,6 +97,8 @@ void test3();
 void test4();
 void test5();
 void test6();
+void test7();
+void test8();
 
 void errorTests();
 
@@ -157,6 +163,8 @@ int main(int argc, char **argv) {
     test4();
     test5();
     test6();
+    test7();
+    test8();
     errorTests();
 
     return 1;
@@ -196,7 +204,7 @@ void test4() {
     // Create a relation with tuples valued 0 to relationSize in random order and perform index tests
     // on attributes of all three types (int, double, string)
     std::cout << "--------------------" << std::endl;
-    std::cout << "createRelationRandom" << std::endl;
+    std::cout << "createRelationNegative" << std::endl;
     createRelationNegative();
     indexTestsNegative();
     deleteRelation();
@@ -206,7 +214,7 @@ void test5() {
     // Create a relation with tuples valued 0 to relationSize in random order and perform index tests
     // on attributes of all three types (int, double, string)
     std::cout << "--------------------" << std::endl;
-    std::cout << "createRelationRandom" << std::endl;
+    std::cout << "createRelationRandomComplex" << std::endl;
     createRelationRandom();
     indexTestsComplex();
     deleteRelation();
@@ -216,8 +224,28 @@ void test6() {
     // Create a relation with tuples valued 0 to relationSize in random order and perform index tests
     // on attributes of all three types (int, double, string)
     std::cout << "--------------------" << std::endl;
-    std::cout << "createRelationRandom" << std::endl;
+    std::cout << "createRelationRandomBig" << std::endl;
     createRelationRandomBig();
+    indexTestsBig();
+    deleteRelation();
+}
+
+void test7() {
+    // Create a relation with tuples valued 0 to relationSize in random order and perform index tests
+    // on attributes of all three types (int, double, string)
+    std::cout << "--------------------" << std::endl;
+    std::cout << "createRelationForwardEven" << std::endl;
+    createRelationForwardEven();
+    indexTestsEven();
+    deleteRelation();
+}
+
+void test8() {
+    // Create a relation with tuples valued 0 to relationSize in random order and perform index tests
+    // on attributes of all three types (int, double, string)
+    std::cout << "--------------------" << std::endl;
+    std::cout << "createRelationForwardBig" << std::endl;
+    createRelationForwardBig();
     indexTestsBig();
     deleteRelation();
 }
@@ -244,6 +272,90 @@ void createRelationForward() {
 
     // Insert a bunch of tuples into the relation.
     for (int i = 0; i < relationSize; i++) {
+        sprintf(record1.s, "%05d string record", i);
+        record1.i = i;
+        record1.d = (double) i;
+        std::string new_data(reinterpret_cast<char *>(&record1), sizeof(record1));
+
+        while (1) {
+            try {
+                new_page.insertRecord(new_data);
+                break;
+            }
+            catch (InsufficientSpaceException e) {
+                file1->writePage(new_page_number, new_page);
+                new_page = file1->allocatePage(new_page_number);
+            }
+        }
+    }
+
+    file1->writePage(new_page_number, new_page);
+}
+
+// -----------------------------------------------------------------------------
+// createRelationForwardBig
+// -----------------------------------------------------------------------------
+
+void createRelationForwardBig() {
+    std::vector <RecordId> ridVec;
+    // destroy any old copies of relation file
+    try {
+        File::remove(relationName);
+    }
+    catch (FileNotFoundException e) {
+    }
+
+    file1 = new PageFile(relationName, true);
+
+    // initialize all of record1.s to keep purify happy
+    memset(record1.s, ' ', sizeof(record1.s));
+    PageId new_page_number;
+    Page new_page = file1->allocatePage(new_page_number);
+
+    // Insert a bunch of tuples into the relation.
+    for (int i = 0; i < relationSizeBig; i++) {
+        sprintf(record1.s, "%05d string record", i);
+        record1.i = i;
+        record1.d = (double) i;
+        std::string new_data(reinterpret_cast<char *>(&record1), sizeof(record1));
+
+        while (1) {
+            try {
+                new_page.insertRecord(new_data);
+                break;
+            }
+            catch (InsufficientSpaceException e) {
+                file1->writePage(new_page_number, new_page);
+                new_page = file1->allocatePage(new_page_number);
+            }
+        }
+    }
+
+    file1->writePage(new_page_number, new_page);
+}
+
+// -----------------------------------------------------------------------------
+// createRelationForwardEven
+// -----------------------------------------------------------------------------
+
+void createRelationForwardEven() {
+    std::vector <RecordId> ridVec;
+    // destroy any old copies of relation file
+    try {
+        File::remove(relationName);
+    }
+    catch (FileNotFoundException e) {
+    }
+
+    file1 = new PageFile(relationName, true);
+
+    // initialize all of record1.s to keep purify happy
+    memset(record1.s, ' ', sizeof(record1.s));
+    PageId new_page_number;
+    Page new_page = file1->allocatePage(new_page_number);
+
+    // Insert a bunch of tuples into the relation.
+    for (int i = 0; i < relationSize*2; i+=2) {
         sprintf(record1.s, "%05d string record", i);
         record1.i = i;
         record1.d = (double) i;
@@ -476,6 +588,21 @@ void indexTests() {
 }
 
 // -----------------------------------------------------------------------------
+// indexTests
+// -----------------------------------------------------------------------------
+
+void indexTestsEven() {
+    if (testNum == 1) {
+        intTestsEven();
+        try {
+            File::remove(intIndexName);
+        }
+        catch (FileNotFoundException e) {
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
 // indexTestsBig
 // -----------------------------------------------------------------------------
 
@@ -536,6 +663,24 @@ void intTests() {
     checkPassFail(intScan(&index, 0, GT, 1, LT), 0)
     checkPassFail(intScan(&index, 300, GT, 400, LT), 99)
     checkPassFail(intScan(&index, 3000, GTE, 4000, LT), 1000)
+}
+
+// -----------------------------------------------------------------------------
+// intTests
+// -----------------------------------------------------------------------------
+
+void intTestsEven() {
+    std::cout << "Create a B+ Tree index on the integer field" << std::endl;
+    BTreeIndex index(relationName, intIndexName, bufMgr, offsetof(tuple, i), INTEGER);
+
+    // run some tests
+    checkPassFail(intScan(&index, 1, GT, 19, LT), 9)
+    checkPassFail(intScan(&index, 20, GTE, 35, LTE), 8)
+    checkPassFail(intScan(&index, -3, GT, 3, LT), 2)
+    checkPassFail(intScan(&index, 996, GT, 1001, LT), 2)
+    checkPassFail(intScan(&index, 0, GT, 1, LT), 0)
+    checkPassFail(intScan(&index, 300, GT, 400, LT), 49)
+    checkPassFail(intScan(&index, 3000, GTE, 4000, LT), 500)
 }
 
 // -----------------------------------------------------------------------------
